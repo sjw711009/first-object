faa54288ff02137295eb3e6c420ddec53fa0ea70
Laffey <sjw711009@163.com>
2021-01-10 19:28:45 +0800

    完成了主页的静态页面

--- assets/js/baseAPI.js
+++ assets/js/baseAPI.js
@@ -3,5 +3,21 @@
 $.ajaxPrefilter(function (option) {
     // 在发起真正的Ajax请求之前，统一拼接请求的根路径
     option.url = 'http://api-breakingnews-web.itheima.net' + option.url;
-    console.log(option.url);
+    // console.log(option.url);
+    // 统一为有权限的接口，设置 headers 请求头
+    if (option.url.indexOf('/my/') !== -1) {
+        option.headers = {
+            Authorization: localStorage.getItem('token') || ''
+        }
+    }
+
+    // 全局挂载 complete 回调函数
+    option.complete = function (res) {
+        if (res.status === 1 && res.responseJSON.message === '身份认证失败') {
+            // 1、强制清空 token
+            localStorage.removeItem('token');
+            // 2、强制跳转到登录页面
+            location.href = '/login.html'
+        }
+    }
 })
